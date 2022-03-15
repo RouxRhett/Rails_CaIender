3640f6b53b4494757faa64982f32b465a86bf1db
RouxRhett <nsch.4871@gmail.com>
2022-03-15 21:58:03 +0900

    [add]ラジオボタンのテスト実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get '/top', to: 'home#top'
+  get '/time', to: 'home#time' 
+  post '/time', to: 'home#post', as: 'tests'
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_29_135532) do
+ActiveRecord::Schema.define(version: 2022_03_15_082416) do
 
   create_table "models", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "tests", force: :cascade do |t|
+    t.boolean "status"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
